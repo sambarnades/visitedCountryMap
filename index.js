fa920e51d10c612e773f565713bc2f8d068e2c7d
Sam B. <smauel.barnades@gmail.com>
2023-11-15 18:54:11 +0100

    Customize error messages but showing a page before redirecting.

--- index.js
+++ index.js
@@ -24,40 +24,69 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
 
-  const results = await db.query("SELECT country_code FROM visited_countries") // Query
-  let total = results.rows.length;
-  // console.log(`There are ${total} entries.`)                     // Control
+  if (res.statusCode == 400) {
+    let error = "Country has already been added, please try again.";
+  } else if(res.statusCode == 404) {
+    let error = "Country doesn't exist, please try again.";
+  }
 
-  results.rows.forEach((country) => {
-    countries.push(country.country_code);
-  });
-  // console.log(results.rows);
-
-  res.render("index.ejs", {                                                         // Rendering
-    countries: countries,
-    total: total
-  })
-
-// console.log(countries)                                                       // Control
+  else {
+    const results = await db.query("SELECT country_code FROM visited_countries") // Query
+    let total = results.rows.length;
+    // console.log(`There are ${total} entries.`)                     // Control
+  
+    results.rows.forEach((country) => {
+      countries.push(country.country_code);
+    });
+    // console.log(results.rows);
+  
+    res.render("index.ejs", {                                                         // Rendering
+      countries: countries,
+      total: total
+    })
+  
+  // console.log(countries)                                                       // Control
+  
+  }
 });
 
 app.post("/add", async (req, res) => {                                                              // POST Route where user enter new visited country  
 
-let country = req.body.country;                                                                     // Catch the user entry
-const newCountry = await db.query(`SELECT * FROM countries WHERE country_name = '${country}' `);    // Query in the countries db which country it is
+  let country = req.body.country;                                                                     // Catch the user entry
+  const newCountry = await db.query(`SELECT * FROM countries WHERE country_name = '${country}' `);    // Query in the countries db which country it is
 
-let newCountryData = [];                                                                            // Prepare a data array to insert into the visited_countries db
-newCountry.rows.forEach(row => {                                                                    // Foreach loop to read the array given by pg
-  newCountryData.push(row.country_code)
-});
+  let newCountryData = [];                                                                            // Prepare a data array to insert into the visited_countries db
+  newCountry.rows.forEach(row => {                                                                    // Foreach loop to read the array given by pg
+    newCountryData.push(row.country_code)
+  });
 
-// console.log(newCountryData);
+  // console.log(newCountryData);
 
-await db.query(`INSERT INTO visited_countries                                                       
-(country_code) VALUES($1)`,
-newCountryData);                                                                                    // Insert the new country into visited_countries db
+try {
+  await db.query(`INSERT INTO visited_countries                                                       
+  (country_code) VALUES($1)`,
+  newCountryData);
 
-res.redirect("/");                                                                                  // Redirect to the main page
+  res.redirect("/");                                                                                  // Redirect to the main page
+
+
+} catch (error) {
+
+  console.log(error);
+
+  if (error.code == "23505") {
+    error = 400
+  } else if (error.code == "42P02") {
+    error = 404
+  }
+
+  res.redirect(error, "/"); // It works but shows a page before redirecting
+
+
+}
+
+                                                                                // Insert the new country into visited_countries db
+
 });
 
 

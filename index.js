046de9e506b2503832de30cab7d7bc8591e7abd8
Sam B. <smauel.barnades@gmail.com>
2023-11-19 17:40:27 +0100

    Succeeded to split errors & show the right message using the try catch function.

--- index.js
+++ index.js
@@ -16,39 +16,40 @@ const db = new pg.Client({
 db.connect();
 
 let countries = [];
+let total;
+let error;
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
+
+
 app.get("/", async (req, res) => {
 
-  if (res.statusCode == 400) {
-    let error = "Country has already been added, please try again.";
-  } else if(res.statusCode == 404) {
-    let error = "Country doesn't exist, please try again.";
-  }
-
-  else {
     const results = await db.query("SELECT country_code FROM visited_countries") // Query
-    let total = results.rows.length;
+    total = results.rows.length;
     // console.log(`There are ${total} entries.`)                     // Control
   
     results.rows.forEach((country) => {
       countries.push(country.country_code);
     });
     // console.log(results.rows);
+
+  res.render("index.ejs", {                                                         // Rendering
+    countries: countries,
+    total: total
+  })
+
   
-    res.render("index.ejs", {                                                         // Rendering
-      countries: countries,
-      total: total
-    })
   
   // console.log(countries)                                                       // Control
   
+ 
+
   }
-});
+);
 
 app.post("/add", async (req, res) => {                                                              // POST Route where user enter new visited country  
 
@@ -72,23 +73,26 @@ try {
 
 } catch (error) {
 
-  console.log(error);
+  // console.log(error);
 
   if (error.code == "23505") {
-    error = 400
-  } else if (error.code == "42P02") {
-    error = 404
-  }
+    error = "The country is already visited, please, try another country."
+    }
+   else if (error.code == "42P02") {
+    error = "Please, try again using a real country name."
+    }
 
-  res.redirect(error, "/"); // It works but shows a page before redirecting
-
+  res.render("index.ejs", {
+    countries: countries,
+    total: total,
+    error: error
+  });
 
 }
 
                                                                                 // Insert the new country into visited_countries db
 
 });
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
